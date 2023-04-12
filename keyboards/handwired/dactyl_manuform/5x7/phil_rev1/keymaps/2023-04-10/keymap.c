7ac330ac78f3ef4d0e5a465823150a22c0f2a59d
philliphall <130250890+philliphall@users.noreply.github.com>
2023-04-12 04:55:11 -0400

    Got OLED working as intended in latest keymap.

--- keyboards/handwired/dactyl_manuform/5x7/phil_rev1/keymaps/2023-04-10/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/phil_rev1/keymaps/2023-04-10/keymap.c
@@ -80,54 +80,83 @@ void keyboard_post_init_user(void) {
 
 // OLED Control
 #ifdef OLED_ENABLE
-bool oled_task_user(void) {
-    // Host Keyboard Layer Status
-    oled_write_P(PSTR("Layer: "), false);
 
-    switch (get_highest_layer(layer_state | default_layer_state)) {
-        case _QWERTY:
-            oled_write_P(PSTR("Default\n"), false);
-            break;
-/*             if (default_layer_state == 0) {
-                oled_write_P(PSTR("Default\n"), false);
-                break;
-            }
-            else if (default_layer_state == 1) {
-                oled_write_P(PSTR("ColemakBase\n"), false);
-                break;
-            }
-            else if (default_layer_state == 2) {
-                oled_write_P(PSTR("Base2\n"), false);
-                break;
-            }
-            else {
-                oled_write_P(PSTR("You got it wrong!\n"), false);
-                break;
-            } */
-		case _COLEMAK: // Doesn't happen because I use this as a base layer, which is not registered the same as other layers.
-			oled_write_P(PSTR("ColemakLayer\n"), false);
-            break;
-        case _FN:
-            oled_write_P(PSTR("Function\n"), false);
-            break;
-        case _NUM:
-            oled_write_P(PSTR("10-Key\n"), false);
-            break;
-		case _GAME:
-			oled_write_P(PSTR("Gaming\n"), false);
-            break;
-        default:
-            // Or use the write_ln shortcut over adding '\n' to the end of your string
-            oled_write_ln_P(PSTR("Undefined"), false);
+static bool oled_logo_on = true;
+oled_rotation_t oled_init_user(oled_rotation_t rotation) { 
+    return OLED_ROTATION_180;
+}
+
+static uint32_t oled_logo_timer;
+void keyboard_post_init_user(void) {
+    oled_logo_timer = timer_read32();
+}
+
+// Logo Definition
+static void render_logo(void) {
+    static const char PROGMEM qmk_logo[] = {
+        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+    };
+
+    oled_write_P(qmk_logo, false);
+}
+
+// Runtime
+bool oled_task_user(void) {
+    
+    // Logo on boot
+    if (timer_elapsed32(oled_logo_timer) < 2000 && oled_logo_on == true) {
+        oled_set_brightness(255);
+        render_logo();
+        return false;
     }
 
-    // Host Keyboard LED Status
-    led_t led_state = host_keyboard_led_state();
-    oled_write_P(led_state.num_lock ? PSTR("    ") : PSTR("-NUM"), false);
-    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
-    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+    else if (timer_elapsed32(oled_logo_timer) < 10000 && oled_logo_on == true) {
+        oled_set_brightness(32);
+        render_logo();
+        return false;
+    }
     
-    return false;
+    // Host Keyboard Layer Status
+    else {
+        if (oled_logo_on) {
+            void oled_clear(void);
+            oled_set_brightness(128);
+            oled_logo_on = false;
+        }
+
+        oled_write_P(PSTR("Layer: "), false);
+
+        switch (get_highest_layer(layer_state | default_layer_state)) {
+            case _QWERTY:
+                oled_write_P(PSTR("Default\n"), false);
+                break;
+            case _COLEMAK: 
+                oled_write_P(PSTR("Colemak\n"), false);
+                break;
+            case _FN:
+                oled_write_P(PSTR("Function\n"), false);
+                break;
+            case _NUM:
+                oled_write_P(PSTR("10-Key\n"), false);
+                break;
+            case _GAME:
+                oled_write_P(PSTR("Gaming\n"), false);
+                break;
+            default:
+                // Or use the write_ln shortcut over adding '\n' to the end of your string
+                oled_write_ln_P(PSTR("Undefined\n"), false);
+        }
+
+        // Host Keyboard LED Status
+        led_t led_state = host_keyboard_led_state();
+        oled_write_P(led_state.num_lock ? PSTR("             \n") : PSTR("Num lock OFF \n"), false);
+        oled_write_P(led_state.caps_lock ? PSTR("CAPS LOCK\n") : PSTR("         \n"), false);
+        oled_write_P(led_state.scroll_lock ? PSTR("  SCR ") : PSTR("      "), false);
+        
+        return false;
+    }
 }
 #endif
 
