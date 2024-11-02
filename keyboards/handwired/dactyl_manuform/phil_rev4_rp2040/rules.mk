a56bf52006ccc62f4cde72f0c940ab5f17d0d91c
philliphall <130250890+philliphall@users.noreply.github.com>
2024-11-02 07:55:09 +0100

    Initial RP2040 Addition - Still working

---
+++ keyboards/handwired/dactyl_manuform/phil_rev4_rp2040/rules.mk
@@ -0,0 +1,17 @@
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = yes        # Console for debug
+COMMAND_ENABLE = yes        # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+SPLIT_KEYBOARD = yes
+OLED_ENABLE = yes
+OLED_DRIVER = ssd1306
+CAPS_WORD_ENABLE = yes
+ENCODER_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = pmw3389
